fc80cd84103700f21770e2c2b3ba73e6a0031ae8
Rosen Karamanov <rosenkaramanov@Rosens-MacBook-Pro.local>
2024-12-22 22:09:17 +0200

    Update header.js

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,15 @@
-export default function Header() {
-  return (
-    <header className="bg-gray-800 text-white py-4">
-      <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-xl font-bold">DevOps Engineer</h1>
-        <nav>
-          <ul className="flex space-x-4">
-            <li><a href="#about" className="hover:text-gray-400">About</a></li>
-            <li><a href="#projects" className="hover:text-gray-400">Projects</a></li>
-            <li><a href="#contact" className="hover:text-gray-400">Contact</a></li>
-          </ul>
-        </nav>
-      </div>
-    </header>
-  );
-}
+import Link from 'next/link';
+
+const Header = () => (
+  <header>
+    <nav>
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/projects">Projects</Link></li>
+      </ul>
+    </nav>
+  </header>
+);
+export default Header;
 
